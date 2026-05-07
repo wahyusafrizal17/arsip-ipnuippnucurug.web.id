8dcc721a248c372b9bcadc80a69d141a9b900751
Wahyu Safrizal <wahyusafrizal174@gmail.com>
2026-05-07 15:17:30 +0700

    ,

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '91ca8b6d4acdb4a26d489318daeb589386a8cf29',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -416,9 +416,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '91ca8b6d4acdb4a26d489318daeb589386a8cf29',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
